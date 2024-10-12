02d7586d2e42a425491016f8603477d4b158cff3
Matheus Gomes Montovani <matheusgmontovani@gmail.com>
2024-10-12 11:49:36 -0300

    d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='titulo'>
+        <h1>Olá, seja bem vindo</h1>
+      </div>
+      <div className='Login'>
+        <div id='campo-email'>
+          <h3>Email</h3>
+          <input id='email' name='myInput' />
+        </div>
+
+        <div id='campo-senha'>
+          <h3>Senha</h3>
+          <input id='senha' name='myInput'/>
+        </div>
+
+        <div>
+          <button 
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </div>
+
+        <div>
+          <a>Cadastrar</a>
+        </div>
+      </div>
     </div>
   );
 }
